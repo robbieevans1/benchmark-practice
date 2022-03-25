fcde96f6290a1145314991e091204f7b42b1774b
robcabobb <robcabobb@gmail.com>
2022-03-25 10:56:05 -0400

    edited sort.js added quick sort

--- sort.js
+++ sort.js
@@ -1,25 +1,23 @@
-const bubbleSort = (arr) => {
-  let sorted = false;
+const quickSort = (arr) => {
+  // use first index as the pivot point
+  const pivot = arr[0];
+  const left = [];
+  const right = [];
 
-  while (!sorted) {
-    sorted = true;
-
-    for (let i = 0; i < arr.length - 1; i++) {
-      // compare current element to next
-      if (arr[i] > arr[i+1]) {
-        // swap using a third temp variable
-        let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
-
-        // flag as not sorted to run loop again
-        sorted = false;
-      }
+  // start at index 1 to ignore pivot
+  for (let i = 1; i < arr.length; i++) {
+    // push into different arrays based on value compared to pivot
+    if (arr[i] <= pivot) {
+      left.push(arr[i]);
+    }
+    else {
+      right.push(arr[i]);
     }
   }
 
-  return arr;
+  // merge arrays and pivot together
+  return left.concat(pivot, right);
 };
 
-
-module.exports = { bubbleSort };
+// export along with bubble sort
+module.exports = { bubbleSort, quickSort };
